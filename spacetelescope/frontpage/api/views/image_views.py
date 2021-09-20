b2186275df9ec7c535651653ccf84c34369da6d3
danialeresbar <daniel.restrepo@unillanos.edu.co>
2021-09-20 12:34:38 -0500

    Create list to exclude images

--- spacetelescope/frontpage/api/views/image_views.py
+++ spacetelescope/frontpage/api/views/image_views.py
@@ -7,6 +7,26 @@ from djangoplicity.media.options import ImageOptions
 from spacetelescope.frontpage.api.serializers import ESASkySerializer
 
 
+EXCLUDED_IMAGES = [
+    'potw1717a',
+    'potw1709a',
+    'potw2021a',
+    'opo1432b',
+    'heic1710d',
+    'potw1024a',
+    'potw1515a',
+    'potw1201a',
+    'potw1215a',
+    'potw2127a',
+    'potw1910a',
+    'potw1149a',
+    'potw1145a',
+    'heic0810ae',
+    'heic1118a',
+    'heic0910h'
+]
+
+
 class ESASkyListView(generics.ListAPIView):
     serializer_class = ESASkySerializer
     renderer_classes = (renderers.JSONRenderer, )
@@ -15,4 +35,6 @@ class ESASkyListView(generics.ListAPIView):
         qs = ImageOptions.Queries.zoomable.queryset(
             Image, ImageOptions, self.request
         )[0].filter(Q(type='Observation') & Q(spatial_quality='Full')).order_by('-priority')
+
+        # TODO: Remove undesired images
         return qs
