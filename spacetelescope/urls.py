07fa2b1c1d24905ff321988452e529786a0ffc11
Alejandro Vivas <alejo.vivas.av@gmail.com>
2024-06-04 10:20:00 -0500

    Contacts app was removed from url and admin panel

--- spacetelescope/urls.py
+++ spacetelescope/urls.py
@@ -19,7 +19,6 @@ from django.views.generic.base import RedirectView
 import djangoplicity.views
 from djangoplicity.announcements.models import Announcement, WebUpdate
 from djangoplicity.announcements.options import AnnouncementOptions, WebUpdateOptions
-from djangoplicity.contacts.views import GroupSubscribe
 from djangoplicity.media.models import Image, Video, PictureOfTheWeek, ImageComparison
 from djangoplicity.media.options import ImageOptions, VideoOptions, PictureOfTheWeekOptions, ImageComparisonOptions
 from djangoplicity.menus.views import sitemap
@@ -114,10 +113,6 @@ urlpatterns += [
 
     url( r'^projects/fits_liberator/fitsimages/', include('djangoplicity.products2.urls.fitsimages'), { 'model': FITSImage, 'options': FITSImageOptions } ),
 
-
-    # Public contacts edit
-    # TODO: Check if these contacts URLs are required
-    url( r'^contacts/', include('djangoplicity.contacts.urls') ),
 
     url( r'^rss/feed.xml$', RedirectView.as_view(url='https://feeds.feedburner.com/hubble_news/') ),
     url( r'^rss/vodcast.xml$', RedirectView.as_view(url='https://feeds.feedburner.com/hubblecast_sd/') ),
