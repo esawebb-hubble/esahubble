dca4eb1b31830beb7c41a0bdeecc7ce908e0fa05
Alejandro Vivas <alejo.vivas.av@gmail.com>
2025-03-10 14:56:40 -0500

    Feed external links were repair

--- hubble/feed_settings.py
+++ hubble/feed_settings.py
@@ -10,67 +10,70 @@
 
 # Feed settings for website project
 
+class ImageFeedSettings():
+    title = 'ESAHubble Images'
+    link = 'https://esahubble.org/images/'
+    external_feed_url = 'https://feeds.feedburner.com/esahubble/images/'
+
 
 class PictureOfTheWeekFeedSettings():
-    external_feed_url = 'https://feeds.feedburner.com/hubble_potw/'
+    title = 'ESAHubble Picture Of The Week'
+    link = 'https://esahubble.org/images/potw/'
+    external_feed_url = 'https://feeds.feedburner.com/esahubble/images/potw/'
+
+
+class Top100FeedSettings():
+    title = 'ESAHubble Top 100 Images'
+    link = 'https://esahubble.org/images/archive/top100'
+    external_feed_url = 'https://feeds.feedburner.com/esahubble/images/top100/'
 
 
 class AnnouncementFeedSettings():
-    title = 'Hubble Announcements'
-    link = 'https://www.esahubble.org/announcements/'
-    external_feed_url = "https://feeds.feedburner.com/hubble_announcements/"
+    title = 'ESAHubble Announcements'
+    link = 'https://esahubble.org/announcements/'
+    external_feed_url = "https://feeds.feedburner.com/esahubble/announcements/"
 
 
-class VideoPodcastFeedSettings():
-    title = 'Esahubble.org Video Feed'
-    link = 'https://www.esahubble.org/videos/'
-    description = 'The Latest Videos from Esahubble.org'
-    enclosure_resources = {
-            '': ['resource_hd_and_apple', 'resource_hd720p_screen', ],
-            'hd': ['resource_hd_and_apple', 'resource_hd720p_screen', ],
-            'sd': ['resource_medium_podcast', 'resource_vodcast', ],
-            'fullhd': ['resource_hd_1080p25_screen', 'resource_hd1080p_screen', ],
-    }
+class VideoFeedSettings():
+    title = 'ESAHubble Videos'
+    link = 'https://esahubble.org/videos/'
+    external_feed_url = 'https://feeds.feedburner.com/esahubble/videos/'
 
 
 class ReleaseFeedSettings():
-    title = 'Hubble News'
-    link = 'https://www.esahubble.org/news/'
-    description = "The latest news about astronomy and the NASA/ESA Hubble Space Telescope"
-    external_feed_url = 'https://feeds.feedburner.com/hubble_news/'
+    title = 'ESAHubble News'
+    link = 'https://esahubble.org/news/'
+    description = "The latest news about astronomy and the NASA/ESA/CSA Hubble Space Telescope"
+    external_feed_url = 'https://feeds.feedburner.com/esahubble/news/'
 
 
 class HubblecastFeedSettings():
-    title = 'Hubblecast %s'
-    link = 'https://www.esahubble.org/videos/hubblecast/'
-    description = 'The latest news about astronomy, space and the NASA/ESA Hubble Space Telescope presented in High Definition is only for devices that play High Definition video (not iPhone or iPod). To watch the Hubblecast on your iPod and/or iPhone, please download the Standard Definition version also available on iTunes.'
+    title = 'ESAHubblecast %s'
+    link = 'https://esahubble.org/videos/hubblecast/'
+    description = ('The latest news about astronomy, space and the NASA/ESA Hubble Space Telescope presented in High '
+                   'Definition is only for devices that play High Definition video (not iPhone or iPod). To watch the '
+                   'Hubblecast on your iPod and/or iPhone, please download the Standard Definition version also '
+                   'available on iTunes.')
     header_template = 'feeds/hubblecast_header.html'
-    external_feed_url = 'https://feeds.feedburner.com/hubblecast/'
+    external_feed_url = 'https://feeds.feedburner.com/esahubble/cast/'
 
 
 class FeedRedirectSettings():
     redirects = {
-                '/images/potw/feed/': PictureOfTheWeekFeedSettings.external_feed_url,
-                '/videos/feed/category/hubblecast/hd/': HubblecastFeedSettings.external_feed_url,
-                '/videos/feed/category/hubblecast/sd/': 'https://feeds.feedburner.com/hubblecast_sd/',
-                '/videos/feed/category/hubblecast/fullhd/': 'https://feeds.feedburner.com/hubblecast_fullhd/',
-                '/videos/feed/category/hubblecast/': HubblecastFeedSettings.external_feed_url,
-                '/news/feed/': ReleaseFeedSettings.external_feed_url,
-                '/announcements/feed/': 'https://feeds.feedburner.com/hubble_announcements/',
+        '/images/feed/': ImageFeedSettings.external_feed_url,
+        '/images/potw/feed/': PictureOfTheWeekFeedSettings.external_feed_url,
+        '/videos/feed/': VideoFeedSettings.external_feed_url,
+        '/videos/feed/category/hubblecast/hd/': HubblecastFeedSettings.external_feed_url,
+        '/videos/feed/category/hubblecast/sd/': 'https://feeds.feedburner.com/esahubble/cast_sd/',
+        '/videos/feed/category/hubblecast/fullhd/': 'https://feeds.feedburner.com/esahubble/cast_fullhd/',
+        '/videos/feed/category/hubblecast/': HubblecastFeedSettings.external_feed_url,
+        '/news/feed/': ReleaseFeedSettings.external_feed_url,
+        '/announcements/feed/': AnnouncementFeedSettings.external_feed_url,
+        '/images/feed/top100/': Top100FeedSettings.external_feed_url,
     }
 
     whitelist = ['FeedBurner', ]  # ,'Mozilla']
     whitelist_ips = ['134.171.', '127.0.0.1']  # must use ?bypass=1 in request
-
-
-class Top100FeedSettings():
-    title = 'Hubble Top 100 Images'
-
-
-class ScienceAnnouncementFeedSettings():
-    title = 'ESA/Hubble Science Announcements'
-    link = 'https://www.esahubble.org/science/announcements/'
-    description = 'ESA/Hubble Science Announcements Feed'
 
 
 CATEGORY_SPECIFIC_SETTINGS = {
